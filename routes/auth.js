290539636e82bed4375192ee59789a8788280399
x-ash-x <palashasati1@gmail.com>
2021-12-16 18:38:37 +0530

    hash password using bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 //register.
 router.post("/register", async (req, res) => {
-	const newUser = new User(req.body);
 	try {
+		//generate new password.
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+		//create new user.
+		const newUser = new User(req.body);
+		//copying hashed password.
+		newUser.password = hashedPassword;
+
+		//save user and repond.
 		const user = await newUser.save();
+
 		res.status(200).json(user);
 	} catch (err) {
 		console.log(err);
