5165f0f93a123a5796e190ee6edaa40570b35dd0
x-ash-x <palashasati1@gmail.com>
2021-12-16 18:15:45 +0530

    create routes, user schema and model

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const helmet = require('helmet')
-
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL,()=>{
@@ -16,6 +17,9 @@ mongoose.connect(process.env.MONGO_URL,()=>{
 app.use(express.json()) //body-parser for post requests.
 app.use(helmet())
 app.use(morgan('common'))
+
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
 
 app.listen(port,()=>{
     console.log("Backend Server Started");
