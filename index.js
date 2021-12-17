31b1b34ae4acd63ceb7e7f5c502f015d056a8fb8
x-ash-x <palashasati1@gmail.com>
2021-12-17 11:39:11 +0530

    create posts model and post routes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL,()=>{
@@ -20,6 +21,7 @@ app.use(morgan('common'))
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(port,()=>{
     console.log("Backend Server Started");
