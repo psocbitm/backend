f672799a958be76889ecd8bb62efeb175b2255da
x-ash-x <palashasati1@gmail.com>
2022-01-03 20:06:37 +0530

    new change

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express =require('express')
-const app = express()
-const port = 8000
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
-const postRoute = require('./routes/posts')
-dotenv.config()
+const express = require("express");
+const app = express();
+const port = 8000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,()=>{
-    console.log("Connected to DB.");
+mongoose.connect(process.env.MONGO_URL, () => {
+	console.log("Connected to DB.");
 });
 
 //middlewares.
-app.use(express.json()) //body-parser for post requests.
-app.use(helmet())
-app.use(morgan('common'))
+app.use(express.json()); //body-parser for post requests.
+app.use(helmet());
+app.use(morgan("common"));
 
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/posts', postRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
-app.listen(port,()=>{
-    console.log("Backend Server Started");
-})
+app.listen(port, () => {
+	console.log("Backend Server Started on port - " + port);
+});
